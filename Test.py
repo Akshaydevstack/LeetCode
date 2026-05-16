0061ebcdcfa854c70844726a7730e52ab3639349
AkshayBharathan <akshay.devstack@gmail.com>
2026-05-16 11:48:04 +0530

    Day9 done

--- Test.py
+++ Test.py
@@ -1,15 +1,2 @@
-
-srt = "malayalm"
-def palindrome(s):
-    cleaned_text = "".join(char.lower() for char in s if char.isalpha())
-
-    left = 0
-    right = len(s)-1
-
-    while left< right:
-        if cleaned_text[left] != cleaned_text[right]:
-            return False
-        
-        return True
-
-print(palindrome(srt))
+def lengthOfLastWord(s):
+        return len(s.strip().split()[-1])
